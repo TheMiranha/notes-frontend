77bae3e4aa562a23b8ac08e486945dedb68985df
TheMiranha <lxbrmiranda@gmail.com>
2022-06-12 16:59:53 -0300

    testing

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Share_Code from './screens/share_code/Share_Code';
 import NotFound from './screens/notFound/NotFound'
 import Profile from './screens/profile/Profile';
 
-const ENDPOINT = 'https://miranha-notes-backend.herokuapp.com/'
+const ENDPOINT = 'https://miranha-notes-backend.herokuapp.com'
 const cookie = new Cookie();
 
 const App = () => {
